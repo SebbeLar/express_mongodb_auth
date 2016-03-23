2d18c0e2d5af5a98bc87b689d48f5be210fa1443
Sebastian Larsson <sebberlarsson@hotmail.com>
2016-03-23 10:18:35 +0100

    Changed comments functionality and eslint rules

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.post('/login', passport.authenticate('local',
         successRedirect: '/campgrounds',
         failureRedirect: '/login'
     }), function (req, res) {
-});
+    });
 
 router.get('/logout', function (req, res) {
     req.logout();
