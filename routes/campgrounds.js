2c69289f3b01b2503ef38707f95bafd51b40aaa4
Sebastian Larsson <sebberlarsson@hotmail.com>
2016-03-23 13:24:28 +0100

    Added update capability for users

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -12,11 +12,15 @@ router.get('/', function (req, res) {
     });
 });
 
-router.post('/', function (req, res) {
+router.post('/', isLoggedIn,function (req, res) {
     var name = req.body.name,
         image = req.body.image,
         desc = req.body.description,
-        obj = {name: name, image: image, description: desc};
+        author = {
+            id: req.user._id,
+            username: req.user.username
+        },
+        obj = {name: name, image: image, description: desc, author: author};
     Campground.create(obj, function (err, result) {
         if (err) {
             console.log('Error of type: ' + err);
@@ -26,7 +30,7 @@ router.post('/', function (req, res) {
     });
 });
 
-router.get('/new', function (req, res) {
+router.get('/new', isLoggedIn,function (req, res) {
     res.render('campgrounds/form');
 });
 
@@ -40,5 +44,33 @@ router.get('/:id', function (req, res) {
         }
     });
 });
+
+router.get('/:id/edit', function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCamp) {
+        if (err) {
+            console.log(err);
+            res.redirect('/campgrounds');
+        } else {
+            res.render('campgrounds/edit', { campground: foundCamp });
+        }
+    });
+});
+
+router.put('/:id', function (req, res) {
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (err, update) {
+        if (err) {
+            res.redirect('/campgrounds');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    })
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 module.exports = router;
