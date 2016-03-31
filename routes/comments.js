d5322d164c5eb7819c54720da09e0efe9a56fb3e
Sebastian Larsson <sebberlarsson@hotmail.com>
2016-03-31 13:34:55 +0200

    added routes for put and destroy request on comments

--- routes/comments.js
+++ routes/comments.js
@@ -35,6 +35,42 @@ router.post('/', isLoggedIn, function (req, res) {
     });
 });
 
+router.get('/:comment_id/edit', function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            res.redirect('back');
+        } else {
+            Comment.findById(req.params.comment_id, function (err, comment) {
+                if (err) {
+                    res.redirect('back');
+                } else {
+                    res.render('comments/edit', {campground: campground, comment: comment});
+                }
+            });
+        }
+    });
+});
+
+router.put('/:comment_id', function (req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, update) {
+        if (err) {
+            res.redirect('/campgrounds');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    })
+});
+
+router.delete('/:comment_id', function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    });
+});
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
