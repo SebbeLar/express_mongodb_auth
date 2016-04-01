4f46c9c00b8482d64f93a0ba6310dd93b4297f01
Sebastian Larsson <sebberlarsson@hotmail.com>
2016-04-01 14:19:46 +0200

    changed to mongolab

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var commentRoutes = require('./routes/comments'),
     campgroundRoutes = require('./routes/campgrounds'),
     indexRoutes = require('./routes/index');
 
-mongoose.connect('mongodb://localhost/yelp_camp');
+// mongoose.connect('mongodb://localhost/yelp_camp'); DEVELOPMENT
+mongoose.connect('mongodb://sebbe:abbaabba@ds011840.mlab.com:11840/campground');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
